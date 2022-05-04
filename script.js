640209ad2594fbb3a9a1696d53f02151fdc3a316
stiginstarview <stiginstarview@gmail.com>
2022-05-04 15:06:44 +0000

    simple interest calculator is functional

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+    // getting values from html into javascript variables
+    var principal = document.getElementById("principal").value;
+
+    //validating the principle amount
+    if(principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    // Calcualting the interest
+    var interest = principal * years * rate /100;
+    // convert number of years into actual years in the future
+    var year = new Date().getFullYear()+parseInt(years);
     
+    // Displaying the result
+    document.getElementById("result").innerHTML= "if you deposit <b>"+principal+"</b>,<br\><br\>at an interest rate of <b>" +rate+"</b>,<br\><br\>You will receive an amount of <b>"+interest+"</b>,<br\><br\>in the year <b>"+year+"</b>";
+
+}
+
+// This function will display the rate amount parallel to the slider.
+function updateRate() {
+    var rateval = document.getElementById("rate").value +"%";
+    document.getElementById("rate_val").innerText=rateval;
 }
         
